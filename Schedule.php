f49cd05bf9dcf88f5cf036c312f4e8aea9a83a78
mst141 <it8anken8@gmail.com>
2020-03-25 22:35:21 +0900

    ページネーションを実装

--- Schedule.php
+++ Schedule.php
@@ -1,11 +1,29 @@
 <?php
     //表示とページネーション
+    define('max_view',4);
     try{
         $db = new PDO('mysql:host=localhost;dbname=SCHEDULE','root','root');
-        // GETで現在のページ数を取得する（未入力の場合は1）
-
-        $sql = 'SELECT DISTINCT * FROM CREATE_INFO';
-        $stmt = $db->query($sql);
+        //必要ページ数を数える
+        $sql = 'SELECT COUNT(*) AS count FROM CREATE_INFO';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $total_count = $stmt->fetch(PDO::FETCH_ASSOC);
+        $pages = ceil($total_count['count'] / max_view);
+        // 現在のページ数を取得
+        if(isset($_GET['page_id'])) {
+            $page = $_GET['page_id'];
+        } else {
+            $page = 1;
+        }
+        $sql = 'SELECT DISTINCT * FROM CREATE_INFO ORDER BY id LIMIT :start,:max';
+        $stmt = $db->prepare($sql);
+        if($page == 1){
+            $stmt->bindValue(":start",$page-1,PDO::PARAM_INT);
+            $stmt->bindValue(":max",max_view,PDO::PARAM_INT);
+        }else{
+            $stmt->bindValue(":start",($page-1)*4,PDO::PARAM_INT);
+            $stmt->bindValue(":max",max_view,PDO::PARAM_INT);
+        }
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_OBJ);
         $stmt = null;
@@ -26,8 +44,8 @@
     </head>
 <body>
     
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Menu</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Menu</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -36,7 +54,7 @@
             <a class="nav-item nav-link active" href="/AdjusTime/Add.php">Add <span class="sr-only">(current)</span></a>
             </div>
         </div>
-        </nav>
+    </nav>
 
         <div class="ScheduleTitle">
             <br><h1>Today's Time Table</h1>
@@ -61,11 +79,11 @@
                     <td><?php echo $value->StarTime; ?></td>
                     <td><?php echo $value->EndTime; ?></td>
                     <td>
-                            <a href="Edit.php?id=<?= $value->id ?>" class="btn btn-primary">Edit</a>
-                        </td>
-                        <td>
-                            <a onclick="return confirm('本当に削除してよろしいですか？')" href="Delete.php?id=<?= $value->id ?>" class="btn btn-danger">Delete</a>
-                        </td>
+                        <a href="Edit.php?id=<?= $value->id ?>" class="btn btn-primary">Edit</a>
+                    </td>
+                    <td>
+                        <a onclick="return confirm('本当に削除してよろしいですか？')" href="Delete.php?id=<?= $value->id ?>" class="btn btn-danger">Delete</a>
+                    </td>
                 </tr>   
             <?php endforeach; ?>
         </table>
@@ -75,18 +93,36 @@
         <div class="mx-auto test-box" style="width: 250px;">
             <nav aria-label="Page navigation example">
                 <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="Schedule.php?id=<?= $value->id ?>">Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <!-- ページネーションを表示 -->
+                    <?php
+                    // for($n=1; $n <= $pages; $n++){
+                    //     if($n == $page){
+                    //         echo "$page";
+                    //     }else{
+                    //         echo "<a href='./Schedule.php?page_id=$n'>$n</a>";
+                    //     }
+                    // }
+                    ?>
+                <!-- Bootstrap4でページネーション表示 -->
+                    <li class="page-item">
+                    <a class="page-link" href="?page_id=<?php echo ($page - 1); ?>" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="?page_id=1">1</a></li>
+                    <li class="page-item"><a class="page-link" href="?page_id=2">2</a></li>
+                    <li class="page-item"><a class="page-link" href="?page_id=3">3</a></li>
+                    <li class="page-item">
+                    <a class="page-link" href="?page_id=<?php echo ($page + 1); ?>" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </li>
                 </ul>
             </nav>
         </div>
     </div>
-
-                
-
 </body>
 
 <div class="LoginFooter">    
